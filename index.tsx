65ad4444f3283022ef9196ab84f57bf9c219bf7c
InfiniteXyy <xuyiyangwing@gmail.com>
2020-12-13 23:10:16 +0800

    support ssr

--- index.tsx
+++ index.tsx
@@ -3,13 +3,15 @@ import type { SvelteComponentDev, SvelteComponent } from "svelte/internal";
 
 const { memo, useEffect, useRef } = React;
 
+const isServer = typeof window === undefined;
+
 export default function linkSvelte<P extends object>(SvelteApp: typeof SvelteComponentDev) {
   return memo<P>((props) => {
     const component = useRef<SvelteComponent>();
-    const ref = useRef<HTMLDivElement>(null)
+    const ref = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-      component.current = new SvelteApp({ target: ref.current!, props });
+      component.current = new SvelteApp({ target: ref.current!, props, hydrate: true });
       return () => component.current && component.current.$destroy();
     }, []);
 
@@ -17,6 +19,9 @@ export default function linkSvelte<P extends object>(SvelteApp: typeof SvelteCom
       if (component.current) component.current.$set(props);
     }, [props]);
 
+    if (isServer && (SvelteApp as any).render) {
+      return <div ref={ref} dangerouslySetInnerHTML={{ __html: (SvelteApp as any).render(props).html }} />;
+    }
     return <div ref={ref} />;
   });
 }
