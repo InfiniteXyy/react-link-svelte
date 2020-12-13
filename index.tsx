f2ce19624f24c3861fe27fe41a513e564dbac65d
InfiniteXyy <xuyiyangwing@gmail.com>
2020-12-13 23:19:21 +0800

    v0.0.2-alpha.2

--- index.tsx
+++ index.tsx
@@ -1,25 +1,23 @@
 import * as React from "react";
-import type { SvelteComponentDev, SvelteComponent } from "svelte/internal";
+import type { SvelteComponent } from "svelte";
 
-const { memo, useEffect, useRef } = React;
+const { memo, useLayoutEffect, useRef } = React;
 
-const isServer = typeof window === undefined;
-
-export default function linkSvelte<P extends object>(SvelteApp: typeof SvelteComponentDev) {
+export default function linkSvelte<P extends object>(SvelteApp: typeof SvelteComponent) {
   return memo<P>((props) => {
     const component = useRef<SvelteComponent>();
     const ref = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
+    useLayoutEffect(() => {
       component.current = new SvelteApp({ target: ref.current!, props, hydrate: true });
       return () => component.current && component.current.$destroy();
     }, []);
 
-    useEffect(() => {
+    useLayoutEffect(() => {
       if (component.current) component.current.$set(props);
     }, [props]);
 
-    if (isServer && (SvelteApp as any).render) {
+    if ((SvelteApp as any).render) {
       return <div ref={ref} dangerouslySetInnerHTML={{ __html: (SvelteApp as any).render(props).html }} />;
     }
     return <div ref={ref} />;
